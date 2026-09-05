037590aa0f060202598473fbf0503fc76102a2ee
Sameer <md.sameer405@gmail.com>
2026-09-05 11:22:25 +0530

    This is sameer code

--- addtoCart.ts
+++ addtoCart.ts
@@ -2,6 +2,9 @@ console.log("Sameer addtoCart.ts file loaded successfully.");
 
 console.log("Adding item to cart...");
 
-//Sameer: Add your logic here to add an item to the cart
+//Sameer: 
+console.log("I am sameer and I am adding an item to the cart.");
+console.log("Item added to cart successfully.");
 
-// Fayaz: You can use the following code snippet to add an item to the cart
+// Fayaz
+//Code
