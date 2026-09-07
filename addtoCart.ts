5fc11e9c2fde8ec233831afb16f1eeff243a5257
Sameer <md.sameer405@gmail.com>
2026-09-07 19:41:30 +0530

    updted the changes in sameer code

--- addtoCart.ts
+++ addtoCart.ts
@@ -1,4 +1,10 @@
-console.log("Sameer addtoCart.ts file loaded successfully.");
+//Exiting code
+// Add to cart
+console.log("AddtoCart.ts file loaded successfully.");
+//Removing from cart
+console.log("Removing from cart Test");
+
+
 
 console.log("Adding item to cart...");
 
@@ -6,5 +12,10 @@ console.log("Adding item to cart...");
 console.log("I am sameer and I am adding an item to the cart.");
 console.log("Item added to cart successfully.");
 
-// Fayaz
+//Abrar 
 //Code
+
+
+
+
+console.log("User Story is created to sameer")
