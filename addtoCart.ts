2b00ff0ceb8fd1eeaa828f348d788e2b7f40e756
Sameer <md.sameer405@gmail.com>
2026-09-07 19:44:03 +0530

    updated the changes in sameer's code

--- addtoCart.ts
+++ addtoCart.ts
@@ -5,3 +5,5 @@ console.log("Adding item to cart...");
 //Sameer: Add your logic here to add an item to the cart
 
 // Fayaz: You can use the following code snippet to add an item to the cart
+
+console.log("Item added to cart successfully.");
